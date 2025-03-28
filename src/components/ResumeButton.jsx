9a47bcef015ab8772b5e640df7190444c95152a8
Zando411 <123271994+Zando411@users.noreply.github.com>
2025-03-28 00:24:59 -0700

    add animations on hover

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,7 +4,8 @@ export default function ResumeButton() {
       <a
         href="/Michael Zandonella Resume.pdf"
         download
-        className="bg-gray-950 p-3 px-4 rounded-xl hover:bg-gray-900 transition-colors duration-200  flex items-center gap-2 outline-1 outline-gray-700"
+        className="bg-gray-950 p-3 px-4 rounded-xl hover:bg-gray-900 transition-colors 
+        duration-200  flex items-center gap-2 outline-1 outline-gray-700 hover:shadow-[0px_0px_30px_20px] hover:shadow-indigo-950/50 hover:ring-2 hover:ring-indigo-900 hover:z-50 group"
       >
         <span className="">Resume</span>
         <svg
@@ -17,7 +18,7 @@ export default function ResumeButton() {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="stroke-white w-5 h-5"
+          className="stroke-white w-5 h-5 group-hover:stroke-indigo-500 group-hover:animate-pulse transition-colors duration-200"
         >
           <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
           <polyline points="7 10 12 15 17 10"></polyline>
