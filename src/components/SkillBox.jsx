c2c9242de9a430d808f97c908c12aa957fec49e4
Zando411 <123271994+Zando411@users.noreply.github.com>
2025-04-09 17:11:02 -0700

    refactor to use more tailwind classes

--- src/components/SkillBox.jsx
+++ src/components/SkillBox.jsx
@@ -10,26 +10,21 @@ export default function SkillBox({ className, skillName, setHovered }) {
 
   const handleMouseLeave = () => {
     setColored(false);
-    setHovered(false);
   };
 
   return (
     <>
       <div
-        className={` bg-gray-950 rounded-xl hover:bg-gray-900 transition-colors duration-200 outline-1 outline-gray-700 text-center p-3 ${
-          colored
-            ? 'shadow-[0px_0px_60px_30px] shadow-indigo-950/50 ring-2 ring-indigo-900 z-50 animate-pulse'
-            : 'shadow-none z-0'
-        }`}
+        className={` bg-gray-950 rounded-xl group hover:bg-gray-900 transition-colors duration-200 outline-1 outline-gray-700 text-center p-3 shadow-none z-0 hover:shadow-[0px_0px_60px_30px] hover:shadow-indigo-950/50 hover:ring-2 hover:ring-indigo-900 hover:z-50`}
         onMouseEnter={() => handleMouseEnter()}
         onMouseLeave={() => handleMouseLeave()}
       >
         <div className="flex flex-col items-center gap-2">
           <div className="icon mt-2">
             <i
-              className={`${className} text-4xl ${
-                colored ? 'colored scale-120' : 'scale-100'
-              } inline-block transition-all duration-200 ease-in-out `}
+              className={`${className} text-4xl text-slate-600 scale-100 group-hover:scale-120 inline-block transition-all duration-200 ease-in-out ${
+                colored ? 'colored' : ''
+              }`}
             />
           </div>
           <p>{skillName}</p>
