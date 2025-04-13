3760e153ee8d5a48972a83ca597c3ae7001d70a5
Michael Zandonella <123271994+zandonella@users.noreply.github.com>
2025-04-13 02:05:05 -0700

    add focus states

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,8 +4,7 @@ export default function ResumeButton() {
       <a
         href="/MichaelZandonellaResume.pdf"
         download
-        className="bg-gray-950 p-3 px-4 rounded-xl hover:bg-gray-900 transition-colors 
-        duration-200  flex items-center gap-2 outline-1 outline-gray-700 hover:shadow-[0px_0px_30px_20px] hover:shadow-indigo-950/50 hover:ring-2 hover:ring-indigo-900 hover:z-50 group"
+        className="group flex items-center gap-2 rounded-xl bg-gray-950 p-3 px-4 outline-1 outline-gray-700 transition-colors duration-200 hover:z-50 hover:bg-gray-900 hover:shadow-[0px_0px_30px_20px] hover:ring-2 hover:shadow-indigo-950/50 hover:ring-indigo-900 focus-visible:z-50 focus-visible:bg-gray-900 focus-visible:shadow-[0px_0px_30px_20px] focus-visible:ring-2 focus-visible:shadow-indigo-950/50 focus-visible:ring-indigo-900"
       >
         <span className="">Resume</span>
         <svg
@@ -18,7 +17,7 @@ export default function ResumeButton() {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="stroke-white w-5 h-5 group-hover:stroke-indigo-500 group-hover:animate-bounceOrig transition-colors duration-200"
+          className="group-hover:animate-bounceOrig h-5 w-5 stroke-white transition-colors duration-200 group-hover:stroke-indigo-500"
         >
           <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
           <polyline points="7 10 12 15 17 10"></polyline>
